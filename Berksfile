2dbcb65bba66ea6b8f1d6279e6977d58c7f6a850
Tasneem Mahmud <tmahmud@spartaglobal.com>
2018-09-26 10:53:41 +0100

    mongo db configured

--- Berksfile
+++ Berksfile
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
 source 'https://supermarket.chef.io'
 
-metadata
+cookbook "node", git: "git@github.com:TasneemMahmud/spartaglobal_node_cookbook.git"
+cookbook "mongo", git: "git@github.com:TasneemMahmud/spartaglobal_mongo_cookbook.git"
--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -11,25 +11,40 @@ describe 'mongo::default' do
     let(:chef_run) do
       # for a complete list of available platforms and versions see:
       # https://github.com/customink/fauxhai/blob/master/PLATFORMS.md
-      runner = ChefSpec::ServerRunner.new(platform: 'ubuntu', version: '16.04')
+      runner = ChefSpec::SoloRunner.new(platform: 'ubuntu', version: '16.04')
       runner.converge(described_recipe)
     end
 
     it 'converges successfully' do
       expect { chef_run }.to_not raise_error
     end
-  end
 
-  context 'When all attributes are default, on CentOS 7.4.1708' do
-    let(:chef_run) do
-      # for a complete list of available platforms and versions see:
-      # https://github.com/customink/fauxhai/blob/master/PLATFORMS.md
-      runner = ChefSpec::ServerRunner.new(platform: 'centos', version: '7.4.1708')
-      runner.converge(described_recipe)
+    it 'updates all sources' do
+      expect(chef_run).to update_apt_update('update')
     end
 
-    it 'converges successfully' do
-      expect { chef_run }.to_not raise_error
+    it 'should add mongo to the sources list' do
+      expect(chef_run).to add_apt_repository('mongodb-org')
+    end
+
+    it 'should install mongodb-org' do
+      expect(chef_run).to upgrade_package 'mongodb-org'
+    end
+
+    it "should create the mongod.conf file in /etc/mongod.conf" do
+      expect(chef_run).to create_template("/etc/mongod.conf")
+    end
+
+    it "should create the mongod.service file in /lib/systemd/system/mongod.service" do
+      expect(chef_run).to create_template("/lib/systemd/system/mongod.service")
+    end
+
+    it "mongod service should be enabled" do
+      expect(chef_run).to enable_service("mongod")
+    end
+
+    it "mongod service should be started" do
+      expect(chef_run).to start_service("mongod")
     end
   end
 end
